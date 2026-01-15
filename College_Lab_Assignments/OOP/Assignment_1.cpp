1db2313b07e9141308619526d4003d4001c5e80f
Tanushree <doddamanitanushree@gmail.com>
2026-01-15 22:51:19 +0530

    Area Calculation

---
+++ College_Lab_Assignments/OOP/Assignment_1.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+void Rarea()
+{   
+    int l,b;
+    cout << "Enter the length: ";
+    cin >> l;
+    cout << "Enter the breadth: ";
+    cin >> b;
+    int area = l*b;
+    cout << "The area of Rectangle is "<< area << endl;
+
+}
+void Sarea()
+{   
+    int s;
+    cout << "Enter the Side: ";
+    cin >> s;
+    int area = s*s;
+    cout << "The area of Square is "<< area << endl;
+
+}
+void Tarea()
+{   
+    int b,h;
+    cout << "Enter the length of base: ";
+    cin >> b;
+    cout << "Enter the length of height: ";
+    cin >> h;
+    int area = ((0.5)*(b*h));
+    cout << "The area of Triangle is "<< area << endl;
+
+}
+int main()
+{
+    int choice;
+    char ch = 'y';
+    while (ch == 'y' || ch == 'Y')
+    {
+        cout << "\n----Area Calculation----";
+        cout << "\n1. Area of Rectangle";
+        cout << "\n2. Area of Square";
+        cout << "\n3. Area of Triangle";
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+
+        switch(choice)
+        {
+            case 1:
+                Rarea();
+                break;
+            
+            case 2:
+                Sarea();
+                break;
+
+            case 3:
+                Tarea();
+                break;
+
+            default:
+                cout << "Invalid choice";
+
+        }
+         cout << "\nDo you want to continue? (y/n): ";
+         cin >> ch;
+    }
+    return 0;
+}
