5332bcc0b988662f3a0fb0c9336a627fd04abd5f
Tanushree <doddamanitanushree@gmail.com>
2026-01-22 17:23:10 +0530

    Still code needs recursive search

--- College_Lab_Assignments/PBL2/Assignment_1.cpp
+++ College_Lab_Assignments/PBL2/Assignment_1.cpp
@@ -1,24 +1,24 @@
 #include<iostream>
-  #include<string>
-  using namespace std;
+#include<string>
+using namespace std;
   class mobileuser
   {
-  string username;
-  string mobileno;
-  string address;
-  public: 
-  	void accept();
- 	void display();
- 	void seqsearch(mobileuser s[], int n, string key);
- };
+    string username;
+    string mobileno;
+    string address;
+    public: 
+        void accept();
+        void display();
+        void seqsearch(mobileuser s[], int n, string key);
+  };
   void mobileuser:: accept ()
   {
-  cout << "Enter username: ";
-  cin >> username;
-  cout << "Enter mobile no: ";
-  cin >> mobileno;
-  cout << "Enter address: ";
-  cin >> address;
+    cout << "Enter username: ";
+    cin >> username;
+    cout << "Enter mobile no: ";
+    cin >> mobileno;
+    cout << "Enter address: ";
+    cin >> address;
   }
   void mobileuser:: display()
   { 
@@ -28,22 +28,22 @@
   }
   void mobileuser :: seqsearch(mobileuser s[], int n, string key)
   {
-  int found = 0;
-  for (int i = 0; i<n; i++)
-  {
-  if (s[i].username==key)
-{
-cout << "\nKey found" << endl;
-s[i].display();
-found++;
-break;
-}
-  }
+    int found = 0;
+    for (int i = 0; i<n; i++)
+    {
+     if (s[i].username==key)
+     {
+        cout << "\nKey found" << endl;
+        s[i].display();
+        found++;
+        break;
+     }
+    }
   
-  if (!found)
-  cout<<"\nKey not found";
+    if (!found)
+    cout<<"\nKey not found";
   
-}
+   }
   int  main ()
   { /*
   int i, n;
@@ -55,20 +55,21 @@ break;
   m.accept();
   m.display();
   }*/
-  mobileuser s1[20];
-  int n, i;
-  string key;
-  cout <<"Enter no. of users: ";
-  cin >> n;
-  for (i = 0; i<n; i++){
-  cout << "\nEnter details for user " << (i + 1) << ":" << endl;
-  s1[i].accept();
-  }
-  cout<<"\n---------Entered Data------------";
-  for (i=0; i<n; i++)
-  s1[i].display();
-  cout<<"\nEnter name to search: ";
-  cin >> key;
-  s1[0].seqsearch(s1,n,key);
-  return 0;
+    mobileuser s1[20];
+    int n, i;
+    string key;
+    cout <<"Enter no. of users: ";
+    cin >> n;
+    for (i = 0; i<n; i++)
+    {
+        cout << "\nEnter details for user " << (i + 1) << ":" << endl;
+        s1[i].accept();
+    }
+    cout<<"\n---------Entered Data------------";
+    for (i=0; i<n; i++)
+        s1[i].display();
+    cout<<"\nEnter name to search: ";
+    cin >> key;
+    s1[0].seqsearch(s1,n,key);
+    return 0;
   }
