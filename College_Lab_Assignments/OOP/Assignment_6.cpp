78840d9bca02488e697323439f69a48ec1cd3eeb
Tanushree <doddamanitanushree@gmail.com>
2026-03-31 20:24:06 +0530

    Assign 6

--- College_Lab_Assignments/OOP/Assignment_6.cpp
+++ College_Lab_Assignments/OOP/Assignment_6.cpp
@@ -40,3 +40,67 @@ int main(){
    return 0;
 
 }
+/*
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+// Student class
+class Student {
+public:
+    int id;
+    string name;
+    int marks;
+
+    void input() {
+        cin >> id >> name >> marks;
+    }
+
+    void display() {
+        cout << id << " " << name << " " << marks << endl;
+    }
+};
+
+int main() {
+    Student s[10];
+    int count = 0;
+
+    // -------- Reading from file --------
+    ifstream fin("input.txt");
+
+    if (!fin) {
+        cout << "Error opening input file!" << endl;
+        return 0;
+    }
+
+    while (fin >> s[count].id >> s[count].name >> s[count].marks) {
+        count++;
+    }
+
+    fin.close();
+
+    // -------- Display Data --------
+    cout << "Data read from file:\n";
+    for (int i = 0; i < count; i++) {
+        s[i].display();
+    }
+
+    // -------- Writing to file --------
+    ofstream fout("output.txt");
+
+    if (!fout) {
+        cout << "Error opening output file!" << endl;
+        return 0;
+    }
+
+    for (int i = 0; i < count; i++) {
+        fout << s[i].id << " " << s[i].name << " " << s[i].marks << endl;
+    }
+
+    fout.close();
+
+    cout << "\nData written to output.txt successfully!" << endl;
+
+    return 0;
+}
+*/
