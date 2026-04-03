ddd55ce28caabc1763b891a897dfc69e5ef499ee
Tanushree <doddamanitanushree@gmail.com>
2026-04-03 21:15:19 +0530

    Quadrant of the Points

---
+++ TLE/Coordinates_of_a_Point.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int x, y;
+   cin >> x >> y;
+   if (x==0 && y==0){
+      cout <<  "Origem"<< endl;
+   }
+   else if(y==0){
+      cout << "Eixo Y"<< endl;
+   }
+   else if(x==0){
+      cout << "Eixo X"<< endl;
+   }
+   else if(x>0 && y>0){
+      cout << "Q1";
+   }
+   else if(x<0 && y>0){
+      cout << "Q2";
+   }
+   else if(x<0 && y<0){
+      cout << "Q3";
+   }
+   else if(x>0 && y<0){
+      cout << "Q4";
+   }
+   return 0;
+}
