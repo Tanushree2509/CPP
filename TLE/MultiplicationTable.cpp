620562bb5a765e4e680ca072bf1e7b75af494504
Tanushree <doddamanitanushree@gmail.com>
2026-06-14 22:35:37 +0530

    Multiplication Tables

---
+++ TLE/MultiplicationTable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+   int i, j, n;
+   cin >> n;
+   for (i =1 ; i<=12; i++)
+   {
+      cout << n <<" * "<< i << " = "<< n*i << endl ;
+   }
+}
